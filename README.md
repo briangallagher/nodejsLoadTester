5a5ba69cfcac4ab3bd9090ab689dde065c5c40e7
Brian Gallagher <brian.gallagher@feedhenry.com>
2016-12-13 14:51:47 +0000

    update

--- README.md
+++ README.md
@@ -5,9 +5,9 @@ curl -X POST -d "seconds=10" http://127.0.0.1:8001/run/cpu
 curl -X POST -d "seconds=2" http://127.0.0.1:8001/run/io
 
 
-curl -X POST -d "seconds=2" http://192.168.99.102:31045/run/io
-curl -X POST -d "seconds=10" http://192.168.99.102:31045/run/cpu
-curl -X POST -d "num=9999999" http://192.168.99.102:31045/run/memory
+curl -X POST -d "seconds=2" http://192.168.99.100:31045/run/io
+curl -X POST -d "seconds=10" http://192.168.99.100:31045/run/cpu
+curl -X POST -d "num=9999999" http://192.168.99.100:31045/run/memory
 
 curl -X POST http://192.168.99.102:31045/run/clearmemory
 
@@ -20,3 +20,25 @@ then needs to go back to a steady state
 High I/O
 
 Careful that it scales back down to the default first !!
+
+
+
+
+
+for i in {1..1000}; do curl -X POST -d "seconds=5" http://192.168.99.100:32628/run/io; done
+only sending in sequence, need to do in parallel
+
+
+
+curl -X POST -d "seconds=1" http://192.168.99.100:32628/run/io
+
+
+autocannon -b "seconds=1" http://192.168.99.100:32628/run/io
+
+autocannon -a 1000 -m 'POST' -b "seconds=0.001" http://192.168.99.100:32628/run/io
+
+autocannon -p 10 -c 1000 -a 100000 -m 'POST' -b "seconds=0.1" http://192.168.99.100:32628/run/io
+
+autocannon -p 10 -c 100 -a 100000 -m 'POST' -b "seconds=0.1" http://192.168.99.100:32628/run/io
+
+autocannon -p 2 -c 2 -a 10000  http://localhost:8001/run/io?seconds=3
--- application.js
+++ application.js
@@ -3,40 +3,14 @@ var express = require('express');
 var mbaasExpress = mbaasApi.mbaasExpress();
 var cors = require('cors');
 require('./lib/stats');
-
 var Prometheus = require("prometheus-client");
 
 var client = new Prometheus();
 
-var counter = client.newCounter({
-  namespace: "counter_test",
-  name: "elapsed_counters_total",
-  help: "The number of counter intervals that have elapsed."
+var responseTimeGauge = client.newGauge({
+  name: "response_time",
+  help: "Response time of most recent request."
 });
-
-var gauge = client.newGauge({
-  namespace: "counter_test",
-  name: "random_number",
-  help: "A random number we occasionally set."
-});
-
-setInterval(function () {
-  counter.increment({
-    period: "1sec" //period is a custom label name in this case with a value of "1sec"
-  });
-}, 1000);
-
-setInterval(function () {
-  counter.increment({
-    period: "2sec" //creating a new series with a period label of "2sec"
-  });
-}, 2000);
-
-setInterval(function () {
-  gauge.set({
-    period: "1sec"
-  }, Math.random() * 1000);
-}, 1000);
 
 // list the endpoints which you want to make securable here
 var securableEndpoints;
@@ -57,7 +31,7 @@ app.use(express.static(__dirname + '/public'));
 // Note: important that this is added just before your own Routes
 app.use(mbaasExpress.fhmiddleware());
 
-app.use('/run', require('./lib/run.js')());
+app.use('/run', require('./lib/run.js')(responseTimeGauge));
 
 app.get("/metrics2", client.metricsFunc());
 
